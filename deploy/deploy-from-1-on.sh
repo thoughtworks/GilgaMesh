c106210e78df51d92e019296f8ee2868b3c22746
Rosalie Tolentino <rtolenti@thoughtworks.com>
2016-02-03 17:02:57 -0800

    Rosalie </3: Cleans unnecessary files in deploy folder

--- deploy/deploy-from-1-on.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NUMBER_OF_DEVICES=`expr $(echo -e "ShowEmuList\nexit\n" | jlinkexe | grep 'J-Link\[' | wc -l) - 1`
-
-FRUITY_REPO=$HOME/Projects/IoT/fruitymesh
-
-for i in $(seq 0 $NUMBER_OF_DEVICES); do
-    echo $i | jlinkexe $FRUITY_REPO/deploy/single-fruitymesh-softdevice-deploy.jlink
-done

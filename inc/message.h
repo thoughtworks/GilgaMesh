a717e2c63465b918201ec44be2122d062ffe2cc4
Yushan Gao <ysgao@thoughtworks.com>
2016-03-10 10:52:09 +0800

    ysgao <3 aligned the message struct,then the data in message will have no invalid spaces

--- inc/message.h
+++ inc/message.h
@@ -10,33 +10,33 @@ typedef enum {
 typedef struct {
 	BleMessageType messageType;	// Message Type is used to identify a request
 	uint8_t password[6];		// Used for checking customer's role,whether the customer can config the device.
-} BleMessageHead;
+}__attribute__ ((packed)) BleMessageHead;
 
 // This is the result of response,every response must declare this result first
 typedef struct {
 	uint8_t errorCode;
-} BleMessageResult;
+}__attribute__ ((packed))  BleMessageResult;
 
 // Query Version Message
 typedef struct {
 	BleMessageHead head;
-} BleMessageQueryVersionReq;
+}__attribute__ ((packed))  BleMessageQueryVersionReq;
 
 typedef struct {
 	BleMessageResult result;
 	uint8_t version;
-} BleMessageQueryVersionRsp;
+}__attribute__ ((packed))  BleMessageQueryVersionRsp;
 
 // DFU Update Message
 typedef struct {
 	BleMessageHead head;
-} BleMessageStartDFUReq;
+}__attribute__ ((packed))  BleMessageStartDFUReq;
 
 typedef struct {
 	BleMessageResult result;
-} BleMessageStartDFURsp;
+}__attribute__ ((packed))  BleMessageStartDFURsp;
 
 typedef struct {
 	BleMessageHead head;
 	uint32_t familyID;
-} BleMessageSetFamilyIDReq;
+}__attribute__ ((packed))  BleMessageSetFamilyIDReq;

aa217694ec7ffbcabde1528e1c4dfdfd880b1e38
Sampo Karvonen <skarvone@thoughtworks.com>
2016-03-31 15:03:42 -0500

    connection.h mock

---
+++ tests/mocks/connection_mock.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "cmocka_includes.h"
+
+void connections_initialize(void){}
+
+void set_central_connection(uint16_t connectionHandle, ble_gap_addr_t deviceAddress){
+   check_expected(connectionHandle);
+}
+
+void set_peripheral_connection(uint16_t connectionHandle, ble_gap_addr_t deviceAddress){
+   check_expected(connectionHandle);
+}
+
+ConnectionType unset_connection(uint16_t connectionHandle){
+   check_expected(connectionHandle);
+   return mock_type(ConnectionType);
+}
+
+void print_all_connections(void){}
+
+connection* find_active_connection_by_address(ble_gap_addr_t address){
+   return mock_ptr_type(connection*);
+}
+
+bool central_connection_active(void){
+   return mock_type(bool);
+}
+
+bool peripheral_connections_active(void){
+   return mock_type(bool);
+}

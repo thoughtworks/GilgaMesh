a336175e3b9f2ef388ac2a4ce674ee8fbd985537
Holly Bowen <hbowen@thoughtworks.com>
2016-04-07 14:03:47 -0700

    Holly </3: removed unused branch of int_to_string code. Also added unit tests

--- src/conversion.c
+++ src/conversion.c
@@ -3,21 +3,11 @@
 char* int_to_string(uint16_t i)
 {
   /* Room for INT_DIGITS digits, - and '\0' */
-  static char buf[INT_DIGITS + 2];
-  char *p = buf + INT_DIGITS + 1;/* points to terminating '\0' */
-  if (i >= 0) {
-    do {
-      *--p = '0' + (i % 10);
-      i /= 10;
-    } while (i != 0);
-    return p;
-  }
-  else {/* i < 0 */
-    do {
-      *--p = '0' - (i % 10);
-      i /= 10;
-    } while (i != 0);
-    *--p = '-';
-  }
+  static char buf[INT_DIGITS + 1];
+  char *p = buf + INT_DIGITS;/* points to terminating '\0' */
+  do {
+    *--p = '0' + (i % 10);
+    i /= 10;
+  } while (i != 0);
   return p;
 }

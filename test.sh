610f855f015ec220a73e072e0cf1365b261b378d
rosatolen <rtolenti@thoughtworks.com>
2016-04-07 20:17:46 -0700

    Rosalie </3: Cleans up separate test runner into go script. Go script will now run all compiled test binaries

--- test.sh
+++
@@ -1,3 +0,0 @@
-gcc -o tests/pn532.out tests/unit/Pn532Test.c -Isrc -Iinc -Ideploy/sdk/nrf_sdk_9_0/components/libraries/util -Ideploy/sdk/nrf_sdk_9_0/components/device -Itests/mocks/sdk/toolchain
-./tests/pn532.out
-rm tests/pn532.out

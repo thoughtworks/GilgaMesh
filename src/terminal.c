9854d3a9a0e725202f404fa1f11e24c7647a128b
Sampo Karvonen <skarvone@thoughtworks.com>
2016-04-07 19:56:59 -0500

    All important ASCII art :)

--- src/terminal.c
+++ src/terminal.c
@@ -18,18 +18,28 @@ void terminal_initialize(void)
   simple_uart_put(27);
   simple_uart_putstring((const uint8_t*) "[H"); //Cursor to Home
 
-  simple_uart_putstring((const uint8_t*) "--------------------------------------------------\r\n");
-  simple_uart_putstring((const uint8_t*) "Terminal started, compile date: ");
+  simple_uart_putstring((const uint8_t*) "------------------------------------------------------\r\n");
+
+   simple_uart_putstring((const uint8_t*) "    __  __        _        __  __        _    \r\n");
+   simple_uart_putstring((const uint8_t*) "   |  \\/  |___ __| |_ _  _|  \\/  |___ __| |_  \r\n");
+   simple_uart_putstring((const uint8_t*) "   | |\\/| / -_|_-< ' \\ || | |\\/| / -_|_-< ' \\ \r\n");
+   simple_uart_putstring((const uint8_t*) "   |_|  |_\\___/__/_||_\\_, |_|  |_\\___/__/_||_| ");
+   simple_uart_putstring((const uint8_t*) "v");
+   simple_uart_putstring((const uint8_t*) int_to_string(APP_VERSION_MAIN));
+   simple_uart_putstring((const uint8_t*) ".");
+   simple_uart_putstring((const uint8_t*) int_to_string(APP_VERSION_SUB));
+   simple_uart_putstring((const uint8_t*) "\r\n");
+   simple_uart_putstring((const uint8_t*) "                        |__/                       \r\n");
+
+
+  simple_uart_putstring((const uint8_t*) "  compile date: ");
   simple_uart_putstring((const uint8_t*) __DATE__);
   simple_uart_putstring((const uint8_t*) " ");
   simple_uart_putstring((const uint8_t*) __TIME__);
-  simple_uart_putstring((const uint8_t*) ", v");
-  simple_uart_putstring((const uint8_t*) int_to_string(APP_VERSION_MAIN));
-  simple_uart_putstring((const uint8_t*) ".");
-  simple_uart_putstring((const uint8_t*) int_to_string(APP_VERSION_SUB));
+
   simple_uart_putstring((const uint8_t*) ", nRF51s ");
   simple_uart_putstring((const uint8_t*) dfu_device_name_with_id());
-  simple_uart_putstring((const uint8_t*) "\r\n--------------------------------------------------\r\n");
+  simple_uart_putstring((const uint8_t*) "\r\n------------------------------------------------------\r\n");
 
   is_terminal_initialized = true;
 }

4bdea3a85fa14e4ed388fdde117415fc46715a45
Holly Bowen <hbowen@thoughtworks.com>
2016-08-09 16:27:25 -0700

    Lauren <3 Holly: added unit tests for HandshakeMessage

--- tests/mocks/gatt_mock.c
+++ tests/mocks/gatt_mock.c
@@ -16,10 +16,15 @@ void send_to_all_connections(uint16_t originHandle, uint8_t *data, uint16_t data
   check_expected(dataLength);
 }
 
+void send_to_single_connection(connection *targetConnection, uint8_t *data, uint16_t dataLength) {
+  check_expected(targetConnection);
+  check_expected(data);
+  check_expected(dataLength);
+}
+
 void gatt_initialize(void) { }
 void propagate_message(ble_gatts_evt_t *gattsEvent) { }
 void send_message_from_connection_queue(connection *targetConnection){ }
-void send_to_single_connection(connection *targetConnection, uint8_t *data, uint16_t dataLength) { }
 void scheduled_broadcast_request(void *data, uint16_t dataLength) { }
 void handle_write_event(void * data, uint16_t dataLength) { }
 
---
+++ tests/unit/message_types/HandshakeMessageTest.c
@@ -0,0 +1,56 @@
+#include "message_types/handshake_message.h"
+
+#include "cmocka_includes.h"
+#include "version.h"
+
+static BleMessageHandshakeReq mockRequest;
+static connection mockConnection;
+
+static int test_setup(void **state) {
+  memset(&mockConnection, 0, sizeof(mockConnection));
+  memset(&mockRequest, 0, sizeof(mockRequest));
+  mockRequest.head.messageType = Handshake;
+  mockRequest.majorVersion = APP_VERSION_MAIN;
+  mockRequest.minorVersion = APP_VERSION_SUB;
+  mockRequest.deviceId = deviceId;
+  return 0;
+}
+
+static void test_send_handshake_message_sends_data_to_one_connection() {
+  expect_value(send_to_single_connection, targetConnection, &mockConnection);
+  expect_memory(send_to_single_connection, data, &mockRequest, sizeof(mockRequest));
+  expect_value(send_to_single_connection, dataLength, sizeof(mockRequest));
+  send_handshake_message(&mockConnection);
+}
+
+static void test_receive_handshake_message_does_not_fail_when_connection_is_not_found() {
+  will_return(find_active_connection_by_handle, NULL);
+
+  assert_int_equal(DoNotPropagate, receive_handshake_message(0, &mockRequest));
+}
+
+static void test_receive_handshake_message_sets_data_for_connection() {
+  will_return(find_active_connection_by_handle, &mockConnection);
+
+  assert_int_equal(DoNotPropagate, receive_handshake_message(0, &mockRequest));
+  assert_int_equal(APP_VERSION_MAIN, mockConnection.majorVersion);
+  assert_int_equal(APP_VERSION_SUB, mockConnection.minorVersion);
+  assert_int_equal(deviceId, mockConnection.deviceId);
+}
+
+int RunHandshakeMessageTest(void) {
+  const struct CMUnitTest tests[] = {
+          cmocka_unit_test_setup(test_send_handshake_message_sends_data_to_one_connection, test_setup),
+          cmocka_unit_test_setup(test_receive_handshake_message_does_not_fail_when_connection_is_not_found, test_setup),
+          cmocka_unit_test_setup(test_receive_handshake_message_sets_data_for_connection, test_setup),
+  };
+
+  return cmocka_run_group_tests_name("HandshakeMessageTest", tests, NULL, NULL);
+}
+
+int main() {
+  return RunHandshakeMessageTest();
+}
+
+
+

10c7138069afebe3dd8c145fb6b881b1916a95ba
Sampo Karvonen <sampo.j.karvonen@gmail.com>
2016-10-18 13:52:31 -0500

    Update setup.sh add NRF52 softdevice

--- setup.sh
+++ setup.sh
@@ -50,46 +50,54 @@ fi
 # ********************* #
 # * GCC ARM Toolchain * #
 # ********************* #
-GCC_ARM_TAR_FILE_MAC=gcc-arm-none-eabi-5_4-2016q2-20160622-mac.tar.bz2
-GCC_ARM_TAR_FILE_LINUX=gcc-arm-none-eabi-5_4-2016q2-20160622-linux.tar.bz2
+GCC_ARM_TAR_FILE_MAC=gcc-arm-none-eabi-5_4-2016q3-20160926-mac.tar.bz2
+GCC_ARM_TAR_FILE_LINUX=gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
 mkdir -p $GCC_ARM_TOOLCHAIN
 #should check checksum
 #should get the latest gcc-arm-none-eabi
 if [ ! -d "$GCC_ARM_TOOLCHAIN/lib/gcc/arm-none-eabi/5.4.1" ]; then
     if [[ $platform == 'osx' ]]; then
-        wget -O $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_MAC https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/$GCC_ARM_TAR_FILE_MAC
+        wget -O $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_MAC https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/$GCC_ARM_TAR_FILE_MAC
         tar -C $GCC_ARM_TOOLCHAIN -xjf $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_MAC
-        mv -f $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q2/* $GCC_ARM_TOOLCHAIN
-        rm -r $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_MAC $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q2/
+        mv -f $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q3/* $GCC_ARM_TOOLCHAIN
+        rm -r $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_MAC $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q3/
     elif [[ $platform == 'linux' ]]; then
-        wget -O $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_LINUX https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/$GCC_ARM_TAR_FILE_LINUX
+        wget -O $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_LINUX https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/$GCC_ARM_TAR_FILE_LINUX
         tar -C $GCC_ARM_TOOLCHAIN -xjf $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_LINUX
-        mv -f $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q2/* $GCC_ARM_TOOLCHAIN
-        rm -r $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_LINUX $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q2/
+        mv -f $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q3/* $GCC_ARM_TOOLCHAIN
+        rm -r $GCC_ARM_TOOLCHAIN/$GCC_ARM_TAR_FILE_LINUX $GCC_ARM_TOOLCHAIN/gcc-arm-none-eabi-5_4-2016q3/
     fi
 fi
 
 # ********************* #
 # * Nordic Softdevice * #
 # ********************* #
-SOFTDEVICE_VERSION=s130_nrf51_2.0.1
+NRF51_SOFTDEVICE_VERSION=s130_nrf51_2.0.1
 if [ ! -f "$SOFTDEVICE_LOCATION/$SOFTDEVICE_VERSION_softdevice.hex" ]; then
     mkdir -p $SOFTDEVICE_LOCATION
-    wget --post-data="confirm_terms_and_conditions=Accept" "https://www.nordicsemi.com/eng/nordic/download_resource/53724/10/9410912" -O $SOFTDEVICE_LOCATION/$SOFTDEVICE_VERSION.zip
-    unzip $SOFTDEVICE_LOCATION/$SOFTDEVICE_VERSION.zip -d $SOFTDEVICE_LOCATION
-    rm $SOFTDEVICE_LOCATION/$SOFTDEVICE_VERSION.zip
+    wget --post-data="confirm_terms_and_conditions=Accept" "https://www.nordicsemi.com/eng/nordic/download_resource/53724/10/9410912" -O $SOFTDEVICE_LOCATION/$NRF51_SOFTDEVICE_VERSION.zip
+    unzip $SOFTDEVICE_LOCATION/$NRF51_SOFTDEVICE_VERSION.zip -d $SOFTDEVICE_LOCATION
+    rm $SOFTDEVICE_LOCATION/$NRF51_SOFTDEVICE_VERSION.zip
+fi
+
+NRF52_SOFTDEVICE_VERSION=s132_nrf52_3.0.0
+if [ ! -f "$SOFTDEVICE_LOCATION/$SOFTDEVICE_VERSION_softdevice.hex" ]; then
+    mkdir -p $SOFTDEVICE_LOCATION
+    wget --post-data="confirm_terms_and_conditions=Accept" "https://www.nordicsemi.com/eng/nordic/download_resource/56261/5/9072884" -O $SOFTDEVICE_LOCATION/$NRF52_SOFTDEVICE_VERSION.zip
+    unzip $SOFTDEVICE_LOCATION/$NRF52_SOFTDEVICE_VERSION.zip -d $SOFTDEVICE_LOCATION
+    rm $SOFTDEVICE_LOCATION/$NRF52_SOFTDEVICE_VERSION.zip
 fi
 
 # ************** #
 # * Nordic SDK * #
 # ************** #
 
-if [ ! -d "$SDK/nrf_sdk_12_0" ]; then
+if [ ! -d "$SDK/nrf_sdk_12_1" ]; then
     mkdir -p $SDK
-    wget -O $SDK/nRF5_SDK_12.0.0_12f24da.zip https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x/nRF5_SDK_12.0.0_12f24da.zip
+    wget -O $SDK/nRF5_SDK_12.1.0_0d23e2a.zip https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x/nRF5_SDK_12.1.0_0d23e2a.zip
     ## should check checksum
-    unzip $SDK/nRF5_SDK_12.0.0_12f24da.zip -d $SDK/nrf_sdk_12_0
-    rm $SDK/nRF5_SDK_12.0.0_12f24da.zip
+    unzip $SDK/nRF5_SDK_12.1.0_0d23e2a.zip -d $SDK/nrf_sdk_12_1
+    rm $SDK/nRF5_SDK_12.1.0_0d23e2a.zip
 fi
 
 if [ ! -d "$DEPLOY_RESOURCES/EHAL" ]; then
@@ -105,12 +113,12 @@ fi
 # ************************* #
 if [ ! -d "$CMOCKA" ]; then
     mkdir -p $CMOCKA
-    wget -O $CMOCKA/cmocka-1.0.1.tar.xz https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-    tar -C $CMOCKA -xvf $CMOCKA/cmocka-1.0.1.tar.xz
-    mv -f $CMOCKA/cmocka-1.0.1/* $CMOCKA
-    mv -f $CMOCKA/cmocka-1.0.1/.[!.]* $CMOCKA
-    rmdir $CMOCKA/cmocka-1.0.1
-    rm $CMOCKA/cmocka-1.0.1.tar.xz
+    wget -O $CMOCKA/cmocka-1.1.0.tar.xz https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz
+    tar -C $CMOCKA -xvf $CMOCKA/cmocka-1.1.0.tar.xz
+    mv -f $CMOCKA/cmocka-1.1.0/* $CMOCKA
+    mv -f $CMOCKA/cmocka-1.1.0/.[!.]* $CMOCKA
+    rmdir $CMOCKA/cmocka-1.1.0
+    rm $CMOCKA/cmocka-1.1.0.tar.xz
 fi
 mkdir -p $CMOCKA/build
 cd $CMOCKA/build
@@ -122,5 +130,5 @@ cd -
 if [[ $platform == 'osx' ]]; then
 	sudo cp `pwd`/deploy/cmocka/lib/libcmocka.0.dylib /usr/local/lib/libcmocka.0.dylib
 elif [[ $platform == 'linux' ]]; then
-	sudo cp `pwd`/deploy/cmocka/lib/libcmocka.so.0.3.1 /usr/local/lib/libcmocka.so.0
+	sudo cp `pwd`/deploy/cmocka/lib/libcmocka.so.0.4.0 /usr/local/lib/libcmocka.so.0
 fi
